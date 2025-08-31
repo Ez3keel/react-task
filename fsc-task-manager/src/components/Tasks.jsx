c49987ecf52cdd544ea296695ce7dedd76a8a3aa
ezequiel <ezequiel.zunesha@gmail.com>
2025-08-31 00:10:57 -0300

    feat: refactor Button component to support variant styling and update Tasks component to utilize new Button props

--- fsc-task-manager/src/components/Tasks.jsx
+++ fsc-task-manager/src/components/Tasks.jsx
@@ -1,27 +1,38 @@
-
 import Button from "./Button";
-import { Trash2 } from 'lucide-react';
+import { Trash2, Plus } from "lucide-react";
 
 const Tasks = () => {
+  return (
+    <div className="px-8 py-16 w-full">
+      <div className="flex w-full justify-between">
+        <div>
+          <span className="text-xs font-semibold text-[#00ADB5]">Minhas Tarefas</span>
+          <h2 className="font-semibold text-xl">Minhas Tarefas</h2>
+        </div>
+      
 
-    return ( 
-     <div className="px-8 py-16">
-           <div className="flex w-full justify-between">
-                <div>
-                    <span className="text-xs font-semibold text-[#00ADB5]">Minhas Tarefas</span>
-                     <h2 className="font-semibold text-xl ">Minhas Tarefas</h2>
-                </div>
-            </div>
+      <div className="flex items-center gap-4">
+        <Button
+          variant="ghost">
+          Limpar tarefa
+          <Trash2 size={16}/>
+        </Button>
 
-            <div>
-                
-                <Button text="Adicionar Tarefa" >
-                     <Trash2/>
-                     Limpar tarefa</Button>
-                <Button>Nova tarefa</Button>
-            </div>
-     </div>
-     );
-}
- 
+        <Button
+        variant="primary">>
+          Nova tarefa
+          <Plus size={16}/>
+        </Button>
+
+
+        {/* <Button 
+          text="Nova tarefa" 
+          RightIcon={Plus} 
+        /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Tasks;
