0f064305a9e1be3b68df77de9b0c77a834d23401
Ezequiel <ezequiel.zunesha@gmail.com>
2025-09-04 14:32:58 -0300

    feat: add animations to AddTaskDialog using react-transition-group

--- fsc-task-manager/src/components/AddTaskDialog.jsx
+++ fsc-task-manager/src/components/AddTaskDialog.jsx
@@ -1,53 +1,73 @@
 import { createPortal } from 'react-dom';
 import Input from './Input';
 import Button from './Button';
+import { CSSTransition } from 'react-transition-group';
+import { useRef } from 'react';
+import './AddTaskDialog.css';
+// import AddTaskDialog from './AddTaskDialog';
 
 const AddTaskDialog = ({ isOpen, handleDialogClose }) => {
-  if (!isOpen) return null;
+  const nodeRef = useRef();
 
-  return createPortal(
-    <div className='fixed bottom-0 left-0 top-0 flex h-screen w-screen items-center justify-center backdrop-blur'>
-      {/* DIALOG */}
-      <div className='rounded-xl bg-white p-5 text-center shadow'>
-        <h2 className='bg-white text-xl font-semibold text-[#35383E]'>
-          Nova tarefa
-        </h2>
-        <p className='mb-4 mt-1 text-sm text-[#9A9C9F]'>
-          Insira as informações a baixo
-        </p>
+  return (
+    // Quando isOpen for true ele exibe o Dialog
+    <CSSTransition
+      nodeRef={nodeRef}
+      in={isOpen}
+      timeout={500}
+      classNames='add-task-dialog'
+      unmountOnExit //Se for false ele tira da tela
+    >
+      <div>
+        {createPortal(
+          <div
+            ref={nodeRef}
+            className='fixed bottom-0 left-0 top-0 flex h-screen w-screen items-center justify-center backdrop-blur'
+          >
+            {/* DIALOG */}
+            <div className='rounded-xl bg-white p-5 text-center shadow'>
+              <h2 className='bg-white text-xl font-semibold text-[#35383E]'>
+                Nova tarefa
+              </h2>
+              <p className='mb-4 mt-1 text-sm text-[#9A9C9F]'>
+                Insira as informações a baixo
+              </p>
 
-        <div className='flex w-[336px] flex-col space-y-4'>
-          <Input
-            id='title'
-            label='Titulo'
-            placeholder='Insira o título da tarefa'
-          />
+              <div className='flex w-[336px] flex-col space-y-4'>
+                <Input
+                  id='title'
+                  label='Titulo'
+                  placeholder='Insira o título da tarefa'
+                />
 
-          <Input id='time' label='Horário' placeholder='Horário' />
+                <Input id='time' label='Horário' placeholder='Horário' />
 
-          <Input
-            id='description'
-            label='Descrição'
-            placeholder='Descreva a tarefa'
-          />
-          <div className='flex gap-3'>
-            <Button
-              size='large'
-              className='w-full'
-              variant='secondary'
-              // Fecha o dialog chama o set
-              onClick={() => handleDialogClose(false)}
-            >
-              Cancelar
-            </Button>
-            <Button size='large' className='w-full'>
-              Salvar
-            </Button>
-          </div>
-        </div>
+                <Input
+                  id='description'
+                  label='Descrição'
+                  placeholder='Descreva a tarefa'
+                />
+                <div className='flex gap-3'>
+                  <Button
+                    size='large'
+                    className='w-full'
+                    variant='secondary'
+                    // Fecha o dialog chama o set
+                    onClick={() => handleDialogClose(false)}
+                  >
+                    Cancelar
+                  </Button>
+                  <Button size='large' className='w-full'>
+                    Salvar
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>,
+          document.body,
+        )}
       </div>
-    </div>,
-    document.body,
+    </CSSTransition>
   );
 };
 
