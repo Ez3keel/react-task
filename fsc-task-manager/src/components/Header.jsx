908cfbd921b42b32a5e27127534aadda944c1f78
Ezequiel <ezequiel.zunesha@gmail.com>
2025-09-10 17:22:08 -0300

    feat: add PropTypes validation to Header component

--- fsc-task-manager/src/components/Header.jsx
+++ fsc-task-manager/src/components/Header.jsx
@@ -1,3 +1,9 @@
+import PropTypes from 'prop-types';
+
 export default function Header(props) {
   return <h1 className="header">{props.children}</h1>;
+}
+
+Header.PropTypes = {
+  children: PropTypes.node.isRequired,
 }
